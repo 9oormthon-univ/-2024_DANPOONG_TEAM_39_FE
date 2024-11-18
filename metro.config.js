43550aae259e1cd12abca1ecb56b98e389158551
gbnam <gbnam453@gmail.com>
2024-11-19 00:00:03 +0900

    Initial commit
    Generated by react-native@0.76.2

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,11 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-const config = {
-  transformer: {
-    // SVG 파일을 변환하기 위해 react-native-svg-transformer 사용
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  },
-  resolver: {
-    // SVG 파일을 asset 대신 컴포넌트로 로드
-    assetExts: getDefaultConfig(__dirname).resolver.assetExts.filter(ext => ext !== 'svg'),
-    sourceExts: [...getDefaultConfig(__dirname).resolver.sourceExts, 'svg'],
-  },
-};
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
