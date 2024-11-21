151df77d686464c8bc939d03e67955010d2c7bda
huiinny <hjara0518@naver.com>
2024-11-22 01:46:06 +0900

    feat: 위클리 타임블록 컴포넌트 구현
    - 타임블록 카테고리 별 색상 지정
    - 공백 블록 생성
    - 공백 alert

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,9 +5,9 @@ import Header from '../components/templates/Header';
 import FamilyList from '../components/organisms/FamilyList';
 import CalendarDatepicker from '../components/molecules/CalendarDatepicker';
 import WeekDays from '../components/organisms/WeekDays';
-import TimeLine from '../components/atoms/TimeLine';
 import TimeBlockList from '../components/organisms/TimeBlockList';
 import MockTasks from '../datas/MockTasks';
+import Profiles from '../datas/Profiles';
 import FloatingButton from '../components/atoms/FloatingButton';
 import moment from 'moment';
 import 'moment/locale/ko';
@@ -20,6 +20,9 @@ const HomeScreen = () => {
 
   // 주 상태 관리
   const [currentWeek, setCurrentWeek] = useState(moment().startOf('week')); // 현재 주의 시작일
+  const weekDates = Array.from({ length: 7 }, (_, i) =>
+    currentWeek.clone().add(i, 'days')
+  );
 
   // FamilyList 컴포넌트의 높이와 투명도 애니메이션 설정
   const familyListHeight = scrollY.interpolate({
@@ -33,17 +36,6 @@ const HomeScreen = () => {
     outputRange: [1, 0],
     extrapolate: 'clamp',
   });
-
-  // 프로필 목록 (예시)
-  const profiles = [
-    { name: '김수한무거', imagePath: require('../assets/images/profile_me.png') },
-    { name: '박수한무거', imagePath: require('../assets/images/profile.png') },
-    { name: '이수한무거북', imagePath: require('../assets/images/profile.png') },
-    { name: '정수한무거북', imagePath: require('../assets/images/profile.png') },
-    { name: '류수한무거', imagePath: require('../assets/images/profile.png') },
-    { name: '최수한무거', imagePath: require('../assets/images/profile.png') },
-    { name: '남수한무거', imagePath: require('../assets/images/profile.png') },
-  ];
 
   // 타임라인 시간 배열
   const hours = Array.from({ length: 24 }, (_, index) => index); // 0 ~ 23 생성
@@ -59,16 +51,16 @@ const HomeScreen = () => {
           { height: familyListHeight, opacity: familyListOpacity },
         ]}
       >
-        <FamilyList profiles={profiles} />
+        <FamilyList Profiles={Profiles} />
       </Animated.View>
 
-      {/* CalendarDatepicker 고정 */}
+      {/* CalendarDatepicker + Weekdays 컴포넌트 */}
       <View style={styles.datePickerContainer}>
         <CalendarDatepicker currentWeek={currentWeek} setCurrentWeek={setCurrentWeek} />
         <WeekDays currentWeek={currentWeek} />
       </View>
 
-      {/* 타임블록 반복 렌더링 */}
+      {/* 일정 렌더링 */}
       <ScrollView
         style={styles.scrollContainer}
         contentContainerStyle={styles.scrollContent}
@@ -78,13 +70,11 @@ const HomeScreen = () => {
         )}
       >
         {/* 위클리 컴포넌트 렌더링 */}
-        <View style={styles.content}>
-          {hours.map((hour) => (
-            <TimeLine key={hour} hour={hour} />
-          ))}
-
-          {/* 일정 블록 렌더링 */}
-          <TimeBlockList tasks={MockTasks} />
+        <View style={styles.weeklyContent}>
+          {/* 타임블록 렌더링 */}
+          <View style={styles.timeblockContainer}>
+            <TimeBlockList tasks={MockTasks} weekDates={weekDates}/>
+          </View>
         </View>
       </ScrollView>
 
@@ -116,8 +106,9 @@ const styles = StyleSheet.create({
   },
   scrollContent: {
     paddingBottom: 80, // 하단 여유 공간 추가
+    zIndex: 1,
   },
-  content: {
+  weeklyContent: {
     paddingLeft: 10,
   },
 });
