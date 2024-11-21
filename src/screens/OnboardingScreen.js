a29a9c469f3f4a39045ffab43f2945ec04bccac8
gbnam <gbnam453@gmail.com>
2024-11-22 01:08:58 +0900

    - 온보딩 1차 최종

--- src/screens/OnboardingScreen.js
+++ src/screens/OnboardingScreen.js
@@ -35,7 +35,7 @@ const slides = [
 ];
 
 const OnboardingScreen = ({ navigation }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [setCurrentIndex] = useState(0);
   const scrollX = useRef(new Animated.Value(0)).current;
 
   // 인디케이터 컴포넌트
