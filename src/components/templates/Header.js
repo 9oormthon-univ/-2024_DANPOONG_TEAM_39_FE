0e9319b4fff71dcf22d788fd179adf8182e23ffe
huiinny <hjara0518@naver.com>
2024-11-26 15:54:58 +0900

    style: icon png->svg파일로 변경
    - bell, menu icon svg 교체
    - header에 수정

--- src/components/templates/Header.js
+++ src/components/templates/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import HeaderBell from '../../assets/images/header_bell.svg';
+import HeaderMenu from '../../assets/images/header_menu.svg';
 
 const Header = () => {
   return (
@@ -8,12 +9,12 @@ const Header = () => {
       <View style={styles.iconContainer}>
         {/* 알림 아이콘 */}
         <TouchableOpacity style={styles.icon}>
-          <Image source={require('../../assets/images/bell.png')} />
+          <HeaderBell />
         </TouchableOpacity>
 
         {/* 메뉴 아이콘 */}
         <TouchableOpacity style={styles.icon}>
-          <Image source={require('../../assets/images/menu.png')} />
+          <HeaderMenu />
         </TouchableOpacity>
       </View>
     </View>
@@ -23,18 +24,18 @@ const Header = () => {
 const styles = StyleSheet.create({
   header: {
     flexDirection: 'row',
-    paddingHorizontal: 10,  // 좌우 여백을 20px로 설정
-    paddingVertical: 16,    // 상하 여백을 16px로 설정
-    backgroundColor: '#FF7F00', // 배경색 추가 (아이콘이 하얀색이라 배경색이 필요)
-    alignItems: 'center',   // 자식 요소를 수직 가운데 정렬
-    justifyContent: 'flex-end', // 아이콘들이 오른쪽 끝으로 정렬되도록 설정
+    paddingHorizontal: 10,
+    paddingVertical: 16,
+    backgroundColor: '#FF7F00',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
   },
   iconContainer: {
-    flexDirection: 'row',   // 아이콘을 가로로 나열
-    alignItems: 'center',   // 아이콘을 수직 가운데 정렬
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   icon: {
-    marginRight: 8,         // 아이콘 간 간격을 8px로 설정
+    marginRight: 8,
   },
 });
 
